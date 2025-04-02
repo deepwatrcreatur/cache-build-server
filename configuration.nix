7fa0da128776fc1fd521238562fa556340249d7c
Anwer Khan <deepwatrcreatur@gmail.com>
2025-04-02 07:42:43 -0400

    added bat and fzf

--- configuration.nix
+++ configuration.nix
@@ -97,6 +97,9 @@
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   neovim
+  helix
+  bat
+  fzf
   wget
   curl  
   iperf3
